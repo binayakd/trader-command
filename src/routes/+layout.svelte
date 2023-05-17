3897994e9a9608a6a3adb3d6d2c67bce0ef4b84f
Binayak Dasgupta <binayakd86@gmail.com>
2023-05-17 16:21:37 +0800

    setup screen

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,41 @@
+<script>
+  import "terminal.css";
+  import "../app.css";
+</script>
+
+
+<div class="container">
+  <div class="terminal-nav">
+    <header class="terminal-logo">
+      <div class="topnav">
+        <div class="logo terminal-prompt">
+          <a class="no-style" href="/">Trader Command</a>
+        </div>
+      </div>
+    </header>
+        
+    <nav class="terminal-menu">
+      <ul vocab="https://schema.org/" typeof="BreadcrumbList">
+        <li>
+          <a href="/" class="menu-item">
+            <span>About</span>
+          </a>
+          <meta property="position">
+        </li>
+        <li property="itemListElement" typeof="ListItem">
+          <a href="https://github.com/binayakd" property="item" typeof="WebPage" class="menu-item" target="_blank" rel="noreferrer">
+            <span property="name">Github</span></a><meta property="position" content="1">
+          </li>
+      </ul>
+    </nav>
+  
+  </div>
+</div>
+
+<div class="container">
+  <main>
+    <slot />
+  </main>
+
+
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="screen">
+    <h1>Welcome to SvelteKit</h1>
+    <p>
+        Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
+    </p>
+</div>
+
+<style>
+    .screen {
+        height: calc(100vh - 91px);
+        /* width: 95vw;  */
+        border: 1px solid #a3abba;
+        margin: 0 auto;
+        padding: calc(var(--global-space) * 2);
+        /* background-image: linear-gradient(to right, transparent 1px, #000 1px), linear-gradient(to bottom, transparent 1px, #000 1px); */
+        background-size: 20px 20px; 
+    }
+</style>
