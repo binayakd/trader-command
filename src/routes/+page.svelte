33239c8d8a1e1d49e8e147e713b7a7645ab9fa98
Binayak Dasgupta <binayakd86@gmail.com>
2023-05-28 15:37:17 +0800

    time wip

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,49 +1,65 @@
 <script>
-    import AgentMain from "../components/agents/AgentMain.svelte";
-    import FleetMain from "../components/fleet/FleetMain.svelte";
-    import ContractsMain from "../components/contracts/ContractsMain.svelte";
-    import StarmapMain from "../components/starmap/StarmapMain.svelte";
+  import AgentMain from "../components/agents/AgentMain.svelte";
+  import FleetMain from "../components/fleet/FleetMain.svelte";
+  import ContractsMain from "../components/contracts/ContractsMain.svelte";
+  import StarmapMain from "../components/starmap/StarmapMain.svelte";
 
-    const screenList = [
-        { name: "Agents", component: AgentMain },
-        { name: "Fleet", component: FleetMain },
-        { name: "Contracts", component: ContractsMain },
-        { name: "Starmap", component: StarmapMain }
-    ]
+  const screenList = [
+    { name: "Agents", component: AgentMain },
+    { name: "Fleet", component: FleetMain },
+    { name: "Contracts", component: ContractsMain },
+    { name: "Starmap", component: StarmapMain },
+  ];
 
-    let selected = AgentMain;
+  let selected = AgentMain;
 </script>
 
 <div class="screen">
-    <div class="screen-nav">
-        {#each screenList as screen }
+  <div class="screen-nav">
+    <div class="left-div">
+      {#each screenList as screen}
         <div class="screen-nav-item">
-            <button
-                class="{selected === screen.component ? "btn btn-primary" : "btn btn-default btn-ghost"}"
-                on:click={() => (selected = screen.component)}
-            >
-                {screen.name}
-            </button>
+          <button
+            class={selected === screen.component
+              ? "btn btn-primary"
+              : "btn btn-default btn-ghost"}
+            on:click={() => (selected = screen.component)}
+          >
+            {screen.name}
+          </button>
         </div>
-        {/each}
+      {/each}
     </div>
+    <div class="utc-time">UTC time</div>
 
-    <svelte:component this={selected} />
+  </div>
+
+
+  <svelte:component this={selected} />
 </div>
 
 <style>
-    .screen {
-        width: 95vw; 
-        margin: 0 auto;
-    }
+  .screen {
+    width: 95vw;
+    margin: 0 auto;
+  }
 
-    .screen-nav {
-        display: flex;
-        /* justify-content: space-between; */
-        /* align-items: center; */
-    }
+  .left-div {
+    align-self: flex-start;
+    display: flex;
+  }
 
-    .screen-nav-item {
-        margin-right: 10px;
-    }
+  .utc-time {
+    align-self: flex-end;
+  }
+
+  .screen-nav {
+    display: flex;
+    justify-content: space-between;
+    /* align-items: center; */
+  }
+
+  .screen-nav-item {
+    margin-right: 10px;
+  }
 </style>
